957c78a6e955150e81ccc31565ae7c1435291d3b
이연경 <inzidang@gmail.com>
2023-07-17 12:52:26 +0900

    123

--- src/app/NotFound.jsx
+++ src/app/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h1 className="font-bold text-[40px]">페이지를 찾을 수 없습니다.</h1>
       <Link to="/">
         <button className="hover:text-neutral-900 hover:font-bold hover:border-2  border border-neutral-800 px-4 py-2 rounded-lg text-neutral-700">
-          홈으로 가기
+          홈으로 가기 확인
         </button>
       </Link>
     </div>
